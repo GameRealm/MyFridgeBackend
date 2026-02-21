55209eddb0bc04ad385a418bd8a1c8e54ca655f8
Oleksandr <159720660+GameRealm@users.noreply.github.com>
2026-02-21 16:23:58 +0200

    Update DTO

--- DTOs/Products/ProductDto.cs
+++ DTOs/Products/ProductDto.cs
@@ -5,6 +5,7 @@ namespace myFridge.DTOs.Products;
 
 public class ProductDto
 {
+    public Guid Id { get; set; }
     public string? Name { get; set; }
     public decimal Quantity { get; set; }
     public string? Unit { get; set; }
